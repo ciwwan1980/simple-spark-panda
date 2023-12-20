35539afc3721b8104b3ebfcd6a93f017680811e8
ciwwan1980 <ajilqado@gmail.com>
2023-12-20 02:36:42 +0100

    Updated the unit prices of products in the products.csv file. The original prices were multiplied by 1.1 to reflect a 10% increase. See the CSV file for the updated information.

--- products.py
+++ products.py
@@ -1,8 +1,8 @@
 import os
 import mysql.connector
+import csv
 from decimal import Decimal
 from dotenv import load_dotenv
-
 
 load_dotenv()
 
@@ -10,7 +10,6 @@ host = os.getenv("DB_HOST")
 user = os.getenv("DB_USER")
 password = os.getenv("DB_PASSWORD")
 database = os.getenv("DB_DATABASE")
-
 
 connection = mysql.connector.connect(
     host=host,
@@ -21,32 +20,34 @@ connection = mysql.connector.connect(
 
 cursor = connection.cursor()
 
-
 select_query = "SELECT * FROM products"
 cursor.execute(select_query)
-products = cursor.fetchall()
+original_products = cursor.fetchall()
 
-print("Data from the 'products' table:")
-for product in products:
-    print(product)
+# Make a copy of the original data
+products = list(original_products)
 
+# Write original data to CSV
+with open('original_data.csv', 'w', newline='') as csvfile:
+    csv_writer = csv.writer(csvfile)
+    csv_writer.writerow(["Product ID", "Name", "Quantity in Stock", "Unit Price"])
+    csv_writer.writerows(original_products)
 
 update_query = "UPDATE products SET unit_price = unit_price * 1.1 WHERE product_id = %s"
 
 for product in products:
     cursor.execute(update_query, (product[0],))
 
-
 connection.commit()
-
 
 cursor.execute(select_query)
 updated_products = cursor.fetchall()
 
-print("\nUpdated data from the 'products' table:")
-for updated_product in updated_products:
-    print(updated_product)
-
+# Write updated data to CSV
+with open('updated_data.csv', 'w', newline='') as csvfile:
+    csv_writer = csv.writer(csvfile)
+    csv_writer.writerow(["Product ID", "Name", "Quantity in Stock", "Unit Price"])
+    csv_writer.writerows(updated_products)
 
 cursor.close()
 connection.close()
