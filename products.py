8d5e8e03cd6f4787a78caf64e74c7e17acf82150
ciwwan1980 <ajilqado@gmail.com>
2023-12-20 02:17:47 +0100

    fixing-products

--- products.py
+++ products.py
@@ -3,14 +3,13 @@
 import os
 from dotenv import load_dotenv
 import mysql.connector
-
 load_dotenv()
+
 
 host = os.getenv("DB_HOST")
 user = os.getenv("DB_USER")
 password = os.getenv("DB_PASSWORD")
 database = os.getenv("DB_DATABASE")
-
 
 connection = mysql.connector.connect(
     host=host,
@@ -19,15 +18,14 @@ connection = mysql.connector.connect(
     database=database
 )
 
-cursor = connection.cursor()
 
+cursor = connection.cursor()
 cursor.execute("SELECT * FROM products")
 result = cursor.fetchall()
 
 print("Data from the 'products' table:")
 for row in result:
     print(row)
-
 
 cursor.close()
 connection.close()
