64f126482d610077e7c6771d0e275fcb90065b13
ciwwan1980 <ajilqado@gmail.com>
2023-12-20 02:30:49 +0100

    update-unit-price

--- products.py
+++ products.py
@@ -1,15 +1,16 @@
-
-
 import os
-from dotenv import load_dotenv
 import mysql.connector
-load_dotenv()
+from decimal import Decimal
+from dotenv import load_dotenv
 
+
+load_dotenv()
 
 host = os.getenv("DB_HOST")
 user = os.getenv("DB_USER")
 password = os.getenv("DB_PASSWORD")
 database = os.getenv("DB_DATABASE")
+
 
 connection = mysql.connector.connect(
     host=host,
@@ -18,14 +19,34 @@ connection = mysql.connector.connect(
     database=database
 )
 
-
 cursor = connection.cursor()
-cursor.execute("SELECT * FROM products")
-result = cursor.fetchall()
+
+
+select_query = "SELECT * FROM products"
+cursor.execute(select_query)
+products = cursor.fetchall()
 
 print("Data from the 'products' table:")
-for row in result:
-    print(row)
+for product in products:
+    print(product)
+
+
+update_query = "UPDATE products SET unit_price = unit_price * 1.1 WHERE product_id = %s"
+
+for product in products:
+    cursor.execute(update_query, (product[0],))
+
+
+connection.commit()
+
+
+cursor.execute(select_query)
+updated_products = cursor.fetchall()
+
+print("\nUpdated data from the 'products' table:")
+for updated_product in updated_products:
+    print(updated_product)
+
 
 cursor.close()
 connection.close()
